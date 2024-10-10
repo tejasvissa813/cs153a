06be3b3f42aaedc6dad6b698a2924c22d3b16bb2
Ran Chen <ranchen@556eci14.coe.engr.ucsb.edu>
2024-10-10 10:38:44 -0700

    inital commit

---
+++ vitis/lab1a_vitis/lab_1a/platform.tcl
@@ -0,0 +1,27 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct C:\Users\ranchen\workspace\lab_1a\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source C:\Users\ranchen\workspace\lab_1a\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {lab_1a}\
+-hw {C:\Users\ranchen\Downloads\project_1\project_1\design_1_wrapper.xsa}\
+-proc {microblaze_0} -os {standalone} -out {C:/Users/ranchen/workspace}
+
+platform write
+platform generate -domains 
+platform active {lab_1a}
+bsp reload
+bsp setdriver -ip sevenSeg_0 -driver generic -ver 3.0 3.1
+bsp write
+bsp reload
+catch {bsp regenerate}
+platform active {lab_1a}
+bsp reload
+platform generate
+bsp write
+platform generate -domains 
---
+++ vivado/lab1a_vivado/project_1/project_1.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -0,0 +1,96 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2022.1 (win64) Build 3526262 Mon Apr 18 15:48:16 MDT 2022
+//Date        : Tue Oct  8 19:30:16 2024
+//Host        : 556eci22 running 64-bit major release  (build 9200)
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   (DDR2_addr,
+    DDR2_ba,
+    DDR2_cas_n,
+    DDR2_ck_n,
+    DDR2_ck_p,
+    DDR2_cke,
+    DDR2_cs_n,
+    DDR2_dm,
+    DDR2_dq,
+    DDR2_dqs_n,
+    DDR2_dqs_p,
+    DDR2_odt,
+    DDR2_ras_n,
+    DDR2_we_n,
+    an,
+    btn,
+    btnCpuReset,
+    clock_rtl,
+    led,
+    seg);
+  output [12:0]DDR2_addr;
+  output [2:0]DDR2_ba;
+  output DDR2_cas_n;
+  output [0:0]DDR2_ck_n;
+  output [0:0]DDR2_ck_p;
+  output [0:0]DDR2_cke;
+  output [0:0]DDR2_cs_n;
+  output [1:0]DDR2_dm;
+  inout [15:0]DDR2_dq;
+  inout [1:0]DDR2_dqs_n;
+  inout [1:0]DDR2_dqs_p;
+  output [0:0]DDR2_odt;
+  output DDR2_ras_n;
+  output DDR2_we_n;
+  output [7:0]an;
+  input [4:0]btn;
+  input btnCpuReset;
+  input clock_rtl;
+  output [15:0]led;
+  output [6:0]seg;
+
+  wire [12:0]DDR2_addr;
+  wire [2:0]DDR2_ba;
+  wire DDR2_cas_n;
+  wire [0:0]DDR2_ck_n;
+  wire [0:0]DDR2_ck_p;
+  wire [0:0]DDR2_cke;
+  wire [0:0]DDR2_cs_n;
+  wire [1:0]DDR2_dm;
+  wire [15:0]DDR2_dq;
+  wire [1:0]DDR2_dqs_n;
+  wire [1:0]DDR2_dqs_p;
+  wire [0:0]DDR2_odt;
+  wire DDR2_ras_n;
+  wire DDR2_we_n;
+  wire [7:0]an;
+  wire [4:0]btn;
+  wire btnCpuReset;
+  wire clock_rtl;
+  wire [15:0]led;
+  wire [6:0]seg;
+
+  design_1 design_1_i
+       (.DDR2_addr(DDR2_addr),
+        .DDR2_ba(DDR2_ba),
+        .DDR2_cas_n(DDR2_cas_n),
+        .DDR2_ck_n(DDR2_ck_n),
+        .DDR2_ck_p(DDR2_ck_p),
+        .DDR2_cke(DDR2_cke),
+        .DDR2_cs_n(DDR2_cs_n),
+        .DDR2_dm(DDR2_dm),
+        .DDR2_dq(DDR2_dq),
+        .DDR2_dqs_n(DDR2_dqs_n),
+        .DDR2_dqs_p(DDR2_dqs_p),
+        .DDR2_odt(DDR2_odt),
+        .DDR2_ras_n(DDR2_ras_n),
+        .DDR2_we_n(DDR2_we_n),
+        .an(an),
+        .btn(btn),
+        .btnCpuReset(btnCpuReset),
+        .clock_rtl(clock_rtl),
+        .led(led),
+        .seg(seg));
+endmodule
